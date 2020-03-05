8082b4525eda6ced415d95f33f79fd6096b44fd8
han-yan-ds <hanprogramming@gmail.com>
2020-03-05 12:44:52 -0700

    Added comments to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const app = express();
 
 // storage directory
 const fs = require('fs');
@@ -13,25 +14,25 @@ if (!fs.existsSync(this.saveDir)) {
   fs.mkdirSync(this.saveDir);
 }
 
+// controllers
 const {
   postImageMetadataController, postImageController
 } = require('./controllers/postController');
 
-const app = express();
-
+// apply middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname));
 app.use(fileUpload());
 
+// routes
 app.post('/images', postImageMetadataController)
 app.post('/saveImage', postImageController)
-
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
+// start server
 app.listen(port, () => {
   console.log(`App is ready at localhost:${port}`);
 });
