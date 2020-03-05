851dd985d8d4a41dc5cd823e9f2afceacc35a160
han-yan-ds <hanprogramming@gmail.com>
2020-03-05 15:33:56 -0700

    Slight refactor of model and controller for metadata so it looks cleaner

--- controllers/getController.js
+++ controllers/getController.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 exports.getImageController = async (req, res) => {
   const {imageId} = req.body;
-  await getImageMetadataModel(imageId, ([data]) => {
+  await getImageMetadataModel(imageId, (data) => {
     try {
       const {imageid, imagename, imagepath} = data;
       // this part SHOULD call getImageModel to grab the actual image from a path
       const location = path.join(imagepath, imagename);
       console.log(location);
       res.send(location);
+      // this part SHOULD call getImageModel to grab the actual image from a path
     } catch (err) {
       throw(err);
-      throw('Couldn\'t find an image with this ID');
     }
   });
 }
--- models/getModel.js
+++ models/getModel.js
@@ -3,9 +3,12 @@ const knex = require('knex')(knexMode);
 
 exports.getImageMetadataModel = async (imageid, cb = data => data) => {
   try {
-    cb(await knex('images').where({
-      imageid
-    }));
+    const results = await knex('images').where({imageid});
+    if (results.length > 0) {
+      cb(results[0])
+    } else {
+      throw Error('Couldn\'t find an image with this ID');
+    }
   } catch (err) {
     console.error("Error grabbing image by ID from database");
     console.error(err);
