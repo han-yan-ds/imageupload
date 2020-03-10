6cce4dbab14c0f7781454eb8ff1ed7032143c082
han-yan-ds <hanprogramming@gmail.com>
2020-03-10 11:22:23 -0600

    Got changing image to work, but now it's callback hell

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', function() {
   document.querySelector('input[type="file"]').addEventListener('change', async function() {
-    await saveImage(this.files[0]);
-    await downloadImage(5, (blob) => changeImage('myImg', blob))
+    saveImage(this.files[0], (imageId) => 
+      downloadImage(imageId, (blob) => 
+        changeImage('myImg', blob)
+      )
+    );
   });
 });
 
@@ -19,12 +22,13 @@ async function downloadImage(imageId, cb = blob => blob) {
   cb(await response.blob());
 }
 
-async function saveImage(file) {
+async function saveImage(file, cb = imageId => imageId) {
   const formData = new FormData();
   formData.append('fileKey', file)
   let response = await fetch('/saveImage', {
     method: 'POST',
     body: formData
   });
-  console.log(await response.json()); // this console.log only shows the structure of the data returned back after uploading
+  const {imageId} = await response.json();
+  cb(imageId);
 }
